a7db97f3b969363a2fb89a5911e4333145402de0
jt174 <jt174@CESAR03>
2023-11-02 12:17:22 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Practica_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Items.AddRange(new object[] { "0", "1", "2", "3", "4", "9", "6", "7", "10", "5", "8" });
+            listBox1.Location = new Point(288, 61);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(94, 279);
+            listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(258, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(325, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Escribe los numeros del 0 al a10";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(427, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 32);
+            button1.TabIndex = 2;
+            button1.Text = "Listar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
